4c4ad8d9f1afa905c95081a608d7659722052f2a
cdrap27 <cdrap27@wgu.edu>
2022-12-17 07:24:52 -0700

    idk

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+
+    @FXML
+    public ChoiceBox<String> language;
+    public TextField username;
+    public TextField password;
+    ObservableList<String> languages  = FXCollections.observableArrayList("English", "French");
+    @FXML
+    private void initialize(){
+        language.setItems(languages);
+    }
+
+
+    public void onLogin(ActionEvent actionEvent) {
+        String user = username.getText();
+        String pass = password.getText();
+
+
+    }
 }
