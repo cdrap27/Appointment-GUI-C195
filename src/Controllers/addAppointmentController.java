13626a79db886c9a7612cb96e8b25bfcc8971df2
cdrap27 <cdrap27@wgu.edu>
2023-01-02 10:20:54 -0700

    added checks to appointment schedule

--- src/Controllers/addAppointmentController.java
+++ src/Controllers/addAppointmentController.java
@@ -36,37 +36,9 @@ public class addAppointmentController {
     }
 
     public void onSave(ActionEvent actionEvent) {
-        Boolean check = true;
-        while(check == true) {
-            check = Model.Appointment.checkName(addTitle);
-            if(check == false)
-                break;
-            check = Model.Appointment.checkDescription(addDescription);
-            if(check == false)
-                break;
-            check = Model.Appointment.checkLocation(addLocation);
-            if(check == false)
-                break;
-            check = Model.Appointment.checkType(addType);
-            if(check == false)
-                break;
-            check = Model.Appointment.checkStartDate(addStartDate);
-            if(check == false)
-                break;
-            check = Model.Appointment.checkEndDate(addEndDate, addStartDate);
-            if(check == false)
-                break;
-            check = Model.Appointment.checkStartTime(addStartTime);
-            if(check == false)
-                break;
-            check = Model.Appointment.checkEndTime(addEndTime, addStartTime);
-            if(check == false)
-                break;
-            check = Model.Appointment.checkAppointmentTime(addStartDate, addEndDate, addStartTime, addEndTime);
-            if(check == false)
-                break;
-            check = false;
-        }
+        Boolean check = checkData();
+
+
     }
 
     public void onCancel(ActionEvent actionEvent) throws IOException {
@@ -84,5 +56,40 @@ public class addAppointmentController {
             stage.getScene().getWindow().centerOnScreen();
             stage.show();
         }
+    }
+
+    public Boolean checkData(){
+        Boolean check = true;
+        while(check == true) {
+            check = Model.Appointment.checkName(addTitle);
+            if (check == false)
+                break;
+            check = Model.Appointment.checkDescription(addDescription);
+            if (check == false)
+                break;
+            check = Model.Appointment.checkLocation(addLocation);
+            if (check == false)
+                break;
+            check = Model.Appointment.checkType(addType);
+            if (check == false)
+                break;
+            check = Model.Appointment.checkStartDate(addStartDate);
+            if (check == false)
+                break;
+            check = Model.Appointment.checkEndDate(addEndDate, addStartDate);
+            if (check == false)
+                break;
+            check = Model.Appointment.checkStartTime(addStartTime);
+            if (check == false)
+                break;
+            check = Model.Appointment.checkEndTime(addEndTime, addStartTime);
+            if (check == false)
+                break;
+            check = Model.Appointment.checkAppointmentTime(addStartDate, addEndDate, addStartTime, addEndTime);
+            if (check == false)
+                break;
+            check = false;
+        }
+        return check;
     }
 }
